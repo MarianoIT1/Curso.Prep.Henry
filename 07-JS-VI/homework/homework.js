991ab2552ad9372712ab5d2bcbcab32183a7d849
Mariano <marianoibarratesta@outlook.com>
2022-05-15 18:04:24 -0300

    homework JS VI ok!

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,21 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var a = nombre[0].toUpperCase();
+  return(a + nombre.slice(1));
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return(cb(n1, n2));
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +26,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var a = 0;
+  numeros.forEach(function(elem) {
+    a = a + elem;
+  });
+  cb(a);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(elem) {
+    cb(elem);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +47,26 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const newArray = [];
+  var a = 0;
+  array.forEach(function(elem, i) {
+    a = cb(elem);
+    newArray[i] = a;
+  });
+  return(newArray);
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const index = [];
+  array.forEach(function(elem) {
+    if (elem[0] === 'a') {
+      index.push(elem);
+    };
+  });
+  return(index);
 }
 
 // No modificar nada debajo de esta línea
